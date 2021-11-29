515ce537edad17926cc0af0a8f899b0021480bd4
ROCKINGAMBUJ <95131970+ROCKINGAMBUJ@users.noreply.github.com>
2021-11-29 11:34:33 +0530

    Delete randomPassword_generator.py

--- randomPassword_generator.py
+++
@@ -1,9 +0,0 @@
-#import random
-#lower="abcdefghijklmnopqrstuvwxyz"
-#upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-#NUMBER="0123456789"
-#Symbol="[]{}#;._-$%"
-#all=lower+upper+NUMBER+Symbol
-#length=9
-#password="".join(random.sample(all,length))
-print("The password you generated is:",password)
