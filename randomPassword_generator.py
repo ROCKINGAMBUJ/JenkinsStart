a0f92abb0edb7aca2922d0c7262c483ef0250fdd
ROCKINGAMBUJ <95131970+ROCKINGAMBUJ@users.noreply.github.com>
2021-11-29 11:21:51 +0530

    Add files via upload

---
+++ randomPassword_generator.py
@@ -0,0 +1,9 @@
+#import random
+#lower="abcdefghijklmnopqrstuvwxyz"
+#upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#NUMBER="0123456789"
+#Symbol="[]{}#;._-$%"
+#all=lower+upper+NUMBER+Symbol
+#length=9
+#password="".join(random.sample(all,length))
+print("The password you generated is:",password)
